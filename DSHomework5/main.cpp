d6db8186f362bd22d251f756fbe030f07c07a27a
apple <yzz0427@vip.qq.com>
2016-10-25 10:56:19 +0800

    First Init

--- DSHomework5/main.cpp
+++ DSHomework5/main.cpp
@@ -7,9 +7,103 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+template<class T>
+class ArrayQueue
+{
+private:
+    int maxSize;
+    int front;
+    int rear;
+    int *queue;
+    int tag;
+public:
+    ArrayQueue(int size)
+    {
+        maxSize=size;
+        queue=new T[maxSize];
+        tag=0;
+        front=0;
+        rear=0;
+    }
+    ~ArrayQueue()
+    {
+        delete [] queue;
+    }
+    void Clear()
+    {
+        tag=0;
+        front=0;
+        rear=0;
+    }
+    bool EnQueue(const T item)
+    {
+        if(tag==1&&rear==front)
+        {
+            std::cout<<"Full Queue"<<std::endl;
+            return false;
+        }
+        if(tag==0)
+            tag=1;
+        queue[rear]=item;
+        rear=(rear+1)%maxSize;
+        return true;
+    }
+    bool DeQueue(T &item)
+    {
+        if(tag==0)
+        {
+            std::cout<<"No Item"<<std::endl;
+            return false;
+        }
+        item=queue[front];
+        if((rear-front)==1||(front-rear)==maxSize-1)
+        {
+            tag=0;
+        }
+        front=(front+1)%maxSize;
+        return true;
+            
+    }
+    bool GetFront(T &item)
+    {
+        if(tag==0)
+        {
+            std::cout<<"No Item"<<std::endl;
+            return false;
+        }
+        item=queue[front];
+        return true;
+    }
+    bool IsEmpty()
+    {
+        if(tag==0)
+            return true;
+        return false;
+    }
+};
+int main(int argc, const char * argv[])
+{
+    ArrayQueue<int> aq(3);
+    aq.EnQueue(1);
+    aq.EnQueue(2);
+    aq.EnQueue(3);
+    aq.EnQueue(4);
+    int temp;
+    while(!aq.IsEmpty())
+    {
+        aq.DeQueue(temp);
+        std::cout<<temp<<std::endl;
+    }
+    aq.DeQueue(temp);
+    aq.Clear();
+    
+    aq.EnQueue(5);
+    aq.EnQueue(6);
+    aq.EnQueue(7);
+    while(!aq.IsEmpty())
+    {
+        aq.DeQueue(temp);
+        std::cout<<temp<<std::endl;
+    }
     return 0;
 }
