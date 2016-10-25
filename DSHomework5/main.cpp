bb11c2872487eaf9bf380e850831e1a3b299c432
apple <yzz0427@vip.qq.com>
2016-10-25 10:06:47 +0800

    Initial Commit

---
+++ DSHomework5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DSHomework5
+//
+//  Created by apple on 2016/10/25.
+//  Copyright © 2016年 YangZhiZhuang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
